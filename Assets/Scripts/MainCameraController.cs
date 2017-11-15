0426e69ab8b00f3ce79dd21b61d4a09af5e76f21
xatazch <pierre.fransson@outlook.com>
2017-11-15 21:57:02 +0100

    städade kod

--- Assets/Scripts/MainCameraController.cs
+++ Assets/Scripts/MainCameraController.cs
@@ -2,19 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainCameraController : MonoBehaviour {
+public class MainCameraController : MonoBehaviour
+{
 
     public GameObject player;
 
     private Vector3 offset;
 
-    void Start () {
-        offset = transform.position - player.transform.position;    
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         transform.position = player.transform.position + offset;
+        //var pos = (Vector2)transform.position;
+
+        //var x = Vector2.Distance(pos, player.transform.position);
+
+        //if (x > 4)
+        //    transform.position = player.transform.position + offset;
 
         //Debug.Log("Player: " + player.transform.position);
         //Debug.Log("offset: " + offset);
