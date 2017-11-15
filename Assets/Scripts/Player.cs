6c465a77b65e408721dbcd120498a1f72d760422
Ted Björklund <ted.bjorklund@outlook.com>
2017-11-15 21:33:15 +0100

    Added player jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,11 @@ public class Player : MonoBehaviour
 {
 
     private Rigidbody2D rigidbody2D;
+    private bool canJump;
     public float speed = 1.5f;
+    public float jumpForce = 5f;
+    public LayerMask groundLayer;
+
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
@@ -16,7 +20,22 @@ public class Player : MonoBehaviour
     {
         var movement = Input.GetAxis("Horizontal");
         rigidbody2D.position += new Vector2(movement * speed * Time.deltaTime, 0);
-        
+
+        if (Input.GetButton("Fire1") && IsGrounded())
+        {
+            rigidbody2D.velocity = Vector2.zero;
+            rigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            canJump = false;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        var hit = Physics2D.Raycast(rigidbody2D.position, Vector2.down, 0.9f, groundLayer);
+        if (hit.collider != null)
+            return true;
+
+        return false;
     }
 
 }
