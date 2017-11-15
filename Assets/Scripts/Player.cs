4b73caf94ccafce7d89f667c8009371c9c0a365a
xatazch <pierre.fransson@outlook.com>
2017-11-15 21:07:56 +0100

    Added speed and delta time

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	private Rigidbody2D rigidbody2D;
+    private Rigidbody2D rigidbody2D;
+    public float speed = 1.5f;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
 
-	void Start() {
-		rigidbody2D = GetComponent<Rigidbody2D>();
-	}
-
-	void Update () {
-		var movement = Input.GetAxis("Horizontal");
-		rigidbody2D.position += new Vector2(movement, 0);
-	}
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        rigidbody2D.position += new Vector2(movement * speed * Time.deltaTime, 0);
+        
+    }
 
 }
