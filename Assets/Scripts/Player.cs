6a15e6c6aae0a4665308802cb39b7f33f1faf1c3
xatazch <pierre.fransson@outlook.com>
2017-11-15 22:24:31 +0100

    Fliped player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,31 @@ public class Player : MonoBehaviour
     public float speed = 1.5f;
     public float jumpForce = 5f;
     public LayerMask groundLayer;
+    private SpriteRenderer spriteRenderer;
+    private bool isFliped = false;
 
     void Start()
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     void Update()
     {
         var movement = Input.GetAxis("Horizontal");
+
         playerRigidbody2D.position += new Vector2(movement * speed * Time.deltaTime, 0);
+
+        if (movement < -0.1)
+        {
+            isFliped = true;
+        }
+        else if (movement > 0.1)
+        {
+            isFliped = false;
+        }
+
+        spriteRenderer.flipX = isFliped;
 
         if (Input.GetButton("Fire1") && IsGrounded())
         {
