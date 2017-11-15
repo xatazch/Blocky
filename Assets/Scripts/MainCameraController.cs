e3fa0111e84d1e22a7b6a2bcab31260705889dd5
xatazch <pierre.fransson@outlook.com>
2017-11-15 21:31:01 +0100

    Added Camera movement

---
+++ Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start () {
+        offset = transform.position - player.transform.position;    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+
+        Debug.Log("Player: " + player.transform.position);
+        Debug.Log("offset: " + offset);
+        Debug.Log("transform.position: " + transform.position);
+    }
+}
